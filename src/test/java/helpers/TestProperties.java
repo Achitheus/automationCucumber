57bc734dacb9093997caf2426743db87ebee56fb
Yuri <yur_yurch@mail.ru>
2023-10-31 00:35:35 +0300

    approach doesnt work

---
+++ src/test/java/helpers/TestProperties.java
@@ -0,0 +1,25 @@
+package helpers;
+
+import org.aeonbits.owner.Config;
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({"system:properties",
+        "system:env",
+        "file:src/test/resources/activeProfile.properties"})
+public interface TestProperties extends Config {
+    @Key("chrome.dir")
+    String chromeDir();
+
+    @Key("chrome.profile.dir")
+    String profileDir();
+
+    @Key("use.chrome.profile")
+    boolean useChromeProfile();
+
+    @Key("active.profile")
+    String activeProfileName();
+
+    @Key("be.headless")
+    boolean beHeadless();
+
+}
